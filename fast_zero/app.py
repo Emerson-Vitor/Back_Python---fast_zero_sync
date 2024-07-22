ab712c65e2d9cafd9c5a18514ac0b94b7db11aec
Emerson-Vitor <vitoremersonpsilva@gmail.com>
2024-07-22 16:15:09 -0300

    remove directory

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,33 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
+
 app = FastAPI()
+database = []
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'olá mundo'}
+
+
+@app.post('/user/', status_code=HTTPStatus.OK, response_model=UserPublic)
+def registration_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+# @app.get('/html', response_class=HTMLResponse)
+# def read_html_return():
+#     return """
+#     <html>
+#         <head>
+#             <title> Meu olá mundo! </title>
+#         </head>
+#         <body>
+#             <h1>Olá mundo</h1>
+#         </body>
+#     </html>"""
